1c1f178211cb2a8817968d1ae68e832cfab3cc06
Andrea Mazzon <mazzon@math.lmu.de>
2023-04-13 07:31:42 +0200

    Added a comment

--- src/main/java/com/andreamazzon/session1/primenumbers/PrimesCheck.java
+++ src/main/java/com/andreamazzon/session1/primenumbers/PrimesCheck.java
@@ -9,11 +9,13 @@ package com.andreamazzon.session1.primenumbers;
 public class PrimesCheck {
 
 	boolean checkIfPrime(int numberToBeChecked) {
-		for (int possibleDivisor = 2; possibleDivisor <= Math.sqrt(numberToBeChecked); possibleDivisor ++) {
-			if (numberToBeChecked % possibleDivisor == 0) {
-				return false;
+
+		for (int possibleDivisor = 2; possibleDivisor <= Math.sqrt(numberToBeChecked); possibleDivisor++) {
+			if (numberToBeChecked % possibleDivisor == 0) {// only if statement, without else
+				return false;// Note here another use of return: it ends the loop and the method
 			}
 		}
 		return true;
 	}
+
 }
